5eb8f2599cfdd4a36e461d83430a974fc77f50f8
Elias <thelli@gmx.at>
2025-11-21 12:02:55 +0100

    added Table

--- VOS_PingPong/ContentView.swift
+++ VOS_PingPong/ContentView.swift
@@ -7,12 +7,20 @@
 
 import SwiftUI
 import RealityKit
+import RealityKitContent
 
 struct ContentView: View {
+    @Environment(AppModel.self) var appModel
 
     var body: some View {
         VStack {
             ToggleImmersiveSpaceButton()
+        }
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(immersiveContentEntity)
+            }
         }
     }
 }
